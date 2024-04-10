5fbe28dcd0a76c9841f27157253b26aae35a4dc8
Pankajsarawag <115498401+Pankajsarawag@users.noreply.github.com>
2024-04-10 13:11:04 +0530

    Update login.tsx

--- client/components/login.tsx
+++ client/components/login.tsx
@@ -6,32 +6,47 @@ import { useState } from "react"
 function Login(props: any) {
   const [loading, setLoading] = useState(false)
 
-
   const login = async () => {
     setLoading(true)
     chrome.identity.getAuthToken({ interactive: true }, async (token) => {
       console.log(token)
-      chrome.storage.local.set({ authToken: token })
-      const res = await fetch("https://www.googleapis.com/oauth2/v1/userinfo", {
 
+      const res = await fetch("https://www.googleapis.com/oauth2/v1/userinfo", {
         headers: {
           Authorization: `Bearer ${token}`
         }
       })
       const data = await res.json()
+      chrome.storage.local.set({ authToken: token, id: data.id })
+      // console.log(data.id)
+
+      try {
+        // console.log(data, "data")
+        axios.post("http://localhost:3000/api/user/register", {
+          id: data.id,
+          data: data
+        })
+      } catch (error) {
+        console.error("Error fetching user data:", error)
+        props.setLogin(true)
+        props.setData(data)
+        setLoading(false)
+      }
+
       if (data.error) {
         console.log(data.error)
         props.setLogin(false)
         setLoading(false)
         return
+      } else {
+        console.log(data)
+        props.setLogin(true)
+        props.setData(data)
+        setLoading(false)
       }
-      console.log(data)
-      props.setLogin(true)
-      props.setData(data)
-      setLoading(false)
     })
   }
-   
+
   return (
     <>
       {loading ? (
